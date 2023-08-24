b62be36fd338cb0032584d7d57278b229db66fa6
Esteban <ecerdas_96@hotmail.com>
2023-08-23 18:06:02 -0600

    Cambiando imagenes footer

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,16 +1,16 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className="footer" style={{backgroundImage: "url(/img/footer.png)"}}>
+    return <footer className="footer" style={{backgroundImage: "url(/img/footer.png)"}} >
         <div className="redes">
             <a href="https://www.facebook.com/esteban.cerdasperez/">
-                <img src="./Img/facebook.png" alt="Facebook"/>
+                <img src="./img/facebook.png" alt="Facebook"/>
             </a>
             <a href="https://www.instagram.com/esteban_c96/?hl=es-la">
-                <img src="./Img/instagram.png" alt="Instagram"/>
+                <img src="/img/instagram.png" alt="Instagram"/>
             </a>
         </div>
-        <img src="./Img/Logo.png" alt="org" />
+        <img src="/img/Logo.png" alt="org" />
         <strong>Desarrollado por Esteban Cerdas</strong>
     </footer>
 }
